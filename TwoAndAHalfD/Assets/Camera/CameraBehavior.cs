106fbad886514ecb74f0e75a191485327df4c165
Gandalf Hudlow <gandalf.hudlow@gmail.com>
2019-06-30 15:19:16 -0700

    Adding TwoAndAHalfD Project

---
+++ TwoAndAHalfD/Assets/Camera/CameraBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    Vector3 cameraStartingPosition;
+    GameObject cameraObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraObject = GameObject.Find("Main Camera");
+        cameraStartingPosition = cameraObject.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraObject.transform.position =
+                new Vector3(
+                       (float)System.Math.Sin(System.Environment.TickCount/1000f)/10 + cameraStartingPosition.x,
+                       (float)System.Math.Cos(System.Environment.TickCount/1000f)/10 + cameraStartingPosition.y,
+                       (float)System.Math.Sin(System.Environment.TickCount/1000f)/10 + cameraStartingPosition.z);
+    }
+}
---
+++ TwoAndAHalfD/Assets/Peg/PegLayoutBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PegLayoutBehavior : MonoBehaviour
+{
+    
+    const float pegsWide = 20;
+    const float pegsTall = 20;
+    void Start()
+    {
+        var pegToCopy = GameObject.Find("Peg");
+        for (var i =0; i < pegsWide; i++)
+        {
+            for (var j = 0; j < pegsTall; j++)
+            {
+                var newPeg = Instantiate(pegToCopy);
+                Color pegColor = CalculateNewPegColorFromGridPosition(i, j);
+                CloneNewPegMaterialAndChangeColor(newPeg, pegColor);
+                CalculateNewPegPosition(i, j, newPeg);
+            }
+        }
+    }
+
+    private static void CalculateNewPegPosition(int i, int j, GameObject newPeg)
+    {
+        var xOffset = 0f;
+        if (j % 2 == 0) {
+            xOffset = 0.5f;
+        }
+        var x = newPeg.transform.position.x + i / 3.0f + 0.01f + xOffset;
+        var y = newPeg.transform.position.y - j / 2.0f;
+
+        var newPegPosition = new Vector3(x, y, newPeg.transform.position.z);
+        newPeg.transform.SetPositionAndRotation(newPegPosition, newPeg.transform.rotation);
+    }
+
+    private static void CloneNewPegMaterialAndChangeColor(GameObject newPeg, Color pegColor)
+    {
+        Material newMaterial = Instantiate(newPeg.GetComponent<Renderer>().sharedMaterial);
+        newMaterial.color = pegColor;
+        newPeg.GetComponent<Renderer>().sharedMaterial = newMaterial;
+    }
+
+    private static Color CalculateNewPegColorFromGridPosition(int i, int j)
+    {
+        return new Color(1 - (i / pegsWide), j / pegsTall, i * j / pegsTall * pegsWide);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
