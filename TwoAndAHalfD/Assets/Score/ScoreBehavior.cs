813ff88d314bb604844ba552ca30c0da139a4321
Gandalf Hudlow <gandalf.hudlow@gmail.com>
2019-07-01 10:34:49 -0700

    Added score and incrementing score when ball hits pegs

---
+++ TwoAndAHalfD/Assets/Score/ScoreBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBehavior : MonoBehaviour
+{
+    TextMeshProUGUI scoreText;
+    public static int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject gameObject = GameObject.Find("ScoreText");
+        
+        scoreText = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var text = "Score: " + score;
+        if (scoreText.text != text) {
+            scoreText.text = text;
+        }
+            
+    }
+
+
+
+
+}
