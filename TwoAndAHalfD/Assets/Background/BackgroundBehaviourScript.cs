5834513b671c726a85559fbb1ac383fafe1a53ea
Max <Maxrapp61531@Gmail.com>
2019-07-04 12:27:53 -0700

    *Made background colors cycle *Ball fixed

---
+++ TwoAndAHalfD/Assets/Background/BackgroundBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehaviourScript : MonoBehaviour
+{
+    GameObject backround;
+    System.Random random;
+    // Start is called before the first frame update
+    void Start()
+    {
+         random = new System.Random();
+        backround = GameObject.Find("Background");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        backround.GetComponent<Renderer>().sharedMaterial.color = new Color((float)random.NextDouble(),(float)random.NextDouble(), (float)random.NextDouble());
+    }
+}
