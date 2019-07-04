39001f5c4acabbb370ee9be0c574fa33e9f2f750
Max <Maxrapp61531@Gmail.com>
2019-07-04 12:43:52 -0700

    *Made game safe for people prone to seizures due to flashing lights (Added half second cool-down on background color)

--- TwoAndAHalfD/Assets/Background/BackgroundBehaviourScript.cs
+++ TwoAndAHalfD/Assets/Background/BackgroundBehaviourScript.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class BackgroundBehaviourScript : MonoBehaviour
 {
+    long lastcolorticks;
     GameObject backround;
     System.Random random;
     // Start is called before the first frame update
     void Start()
     {
          random = new System.Random();
+        lastcolorticks = System.Environment.TickCount;
         backround = GameObject.Find("Background");
     }
 
     // Update is called once per frame
     void Update()
     {
-    
-        backround.GetComponent<Renderer>().sharedMaterial.color = new Color((float)random.NextDouble(),(float)random.NextDouble(), (float)random.NextDouble());
+        long delta = System.Environment.TickCount - lastcolorticks;
+        if (delta > 500)
+        {
+            lastcolorticks = System.Environment.TickCount;
+            backround.GetComponent<Renderer>().sharedMaterial.color = new Color((float)random.NextDouble(),(float)random.NextDouble(), (float)random.NextDouble());
+        }
+
     }
 }
