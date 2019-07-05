72b0adf940ee605bafe08d5870da5c3061a0186a
Gandalf Hudlow <gandalf.hudlow@gmail.com>
2019-07-05 10:24:28 -0700

    Added Spinner that throws ball back up to the top Added logic to reset ball if it ends up under the floor

--- TwoAndAHalfD/Assets/Ball/BallBehaviourScript.cs
+++ TwoAndAHalfD/Assets/Ball/BallBehaviourScript.cs
@@ -6,20 +6,28 @@ public class BallBehaviourScript : MonoBehaviour
 {
     GameObject ball;
     System.Random random;
+    Vector3 startingBallPosition;
+    GameObject floor;
+
+    enum BallState
+    {
+        Bouncing,
+        SpinnerMoving,
+    }
+
+    BallState ballState = BallState.Bouncing;
 
     // Start is called before the first frame update
     void Start()
     {
-     ball  = GameObject.Find("Ball");
-       random = new System.Random();
+        ball  = GameObject.Find("Ball");
+        random = new System.Random();
+        startingBallPosition = ball.transform.position;
         ball.transform.position = new Vector3(ball.transform.position.x + (float)random.NextDouble() * 6, ball.transform.position.y, ball.transform.position.z);
+
+        floor = GameObject.Find("Floor");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
 
     private void OnTriggerEnter(Collider collider)
 
@@ -51,7 +59,7 @@ public class BallBehaviourScript : MonoBehaviour
     }
 
 
-
+    
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -62,8 +70,54 @@ public class BallBehaviourScript : MonoBehaviour
 
             ball.GetComponent<PhysicMaterial>().bounciness = (float) random.NextDouble();
         }
+        if (collision.collider.name == "Spinner" 
+            || collision.collider.name == "Spinner2")
+        {
+            ballState = BallState.SpinnerMoving;
+            var ballRigidBody = ball.GetComponent<Rigidbody>();
+            ballRigidBody.useGravity = false;
+            ballRigidBody.constraints = ballRigidBody.constraints & ~RigidbodyConstraints.FreezePositionZ;
+            spinnerStart = System.Environment.TickCount;
+            spinnerBallHitStartPosition = ball.transform.position;
+        }
+    }
 
-        
-       
+    long spinnerStart = 0;
+    Vector3 spinnerBallHitStartPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        if (ballState == BallState.Bouncing)
+        {
+            FixBadBallStartingPosition();
+        }
+        else if (ballState == BallState.SpinnerMoving)
+        {
+            long delta = System.Environment.TickCount - spinnerStart;
+            float deltaSeconds = delta / 1000f;
+            var initialZVelocity = -3f;
+            var initialYVelocity = 2f;
+            var zPosition = spinnerBallHitStartPosition.z + initialZVelocity * deltaSeconds + 1f * deltaSeconds * deltaSeconds;
+            var yPosition = spinnerBallHitStartPosition.y + initialYVelocity*deltaSeconds;
+
+            ball.transform.position = new Vector3(ball.transform.position.x, yPosition, zPosition);
+
+            if (ball.transform.position.y >= startingBallPosition.y)
+            {
+                var ballRigidBody = ball.GetComponent<Rigidbody>();
+                ballRigidBody.useGravity = true;
+                ball.transform.position = new Vector3(spinnerBallHitStartPosition.x, ball.transform.position.y, spinnerBallHitStartPosition.z);
+                ballRigidBody.constraints = ballRigidBody.constraints | RigidbodyConstraints.FreezePositionZ;
+                ballState = BallState.Bouncing;
+            }
+        }
+    }
+
+    private void FixBadBallStartingPosition()
+    {
+        if (ball.transform.position.y < floor.transform.position.y)
+        {
+            ball.transform.position = startingBallPosition;
+        }
     }
 }
